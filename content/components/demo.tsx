865d8106bba6a0a8782acec95da808cc68747089
testwak <braveidk@gmail.com>
2025-05-17 21:17:01 +0700

    changes

--- content/components/demo.tsx
+++ content/components/demo.tsx
@@ -3,7 +3,7 @@ import {
     AccordionContent,
     AccordionItem,
     AccordionTrigger,
-} from "docs/components/ui/accordion";
+} from "@/components/ui/accordion";
 
 interface AccordionEntry {
     title: string;
